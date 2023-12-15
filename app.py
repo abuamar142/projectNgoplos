3947c5fa611817704e28dbf5a301ecb1c07607e1
La0deArushidayat <hidayatlaodearus@gmail.com>
2023-12-15 02:24:05 -0800

    task terakhir

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
+admin_username = 'admin'
+admin_password = 'admin123'
+
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -31,9 +34,7 @@ def register():
         username = request.form.get('username') 
         password = request.form.get('password')
         role = request.form.get('role')
-        # Menggunakan kata sandi langsung tanpa hashing
         data = (username, password, role)
-        # Cek username atau email
         user_instance = user()
         result = user_instance.check_username_or_password(username, password)
         if result is None:
@@ -48,7 +49,16 @@ def register():
 @app.route('/loggeddin')
 def loggeddin():
     if 'loggedin' in session:
-        return render_template('home_login_failed.html')
+        if session['role'] == 'admin':
+            return redirect(url_for('all_cafes'))  
+        elif session['role'] == 'pemilik_cafe':
+            if 'user_id' in session:
+                user_id = session['user_id']
+                return redirect(url_for('get_cafe_and_menu_by_user_id', user_id=user_id))
+            else:
+                return render_template('home_login_failed.html')  # or redirect somewhere else
+        else:
+            return render_template('home_login_failed.html')
     flash('Harap Login dulu', 'danger')
     return redirect(url_for('login'))
 
@@ -59,19 +69,28 @@ def login():
         password = request.form.get('password')
 
         user_instance = user()
-        result = user_instance.check_username(username)
+        result = user_instance.check_username_or_password(username, password) 
 
         if result is None:
-            flash('Login Gagal, Cek Username Anda', 'danger')
-            return redirect(url_for('login_failed'))
-        elif result[2] != password: 
-            flash('Login gagal, Cek Password Anda', 'danger')
-            return redirect(url_for('login_failed'))
+            if username == admin_username and password == admin_password: 
+                session['loggedin'] = True
+                session['username'] = username
+                session['role'] = 'admin'
+                return redirect(url_for('loggeddin'))
+            else:
+                flash('Login Gagal, Cek Username atau Password Anda', 'danger')
+                return redirect(url_for('login_failed'))
         else:
             session['loggedin'] = True
+            session['user_id'] = result[0]
             session['username'] = result[2]
             session['role'] = result[3]
-            return redirect(url_for('loggeddin'))
+
+            if result[3] == 'pemilik_cafe':
+                return redirect(url_for('get_cafe_and_menu_by_user_id', user_id=result[0]))
+            else:
+                return redirect(url_for('loggeddin'))
+    
     return render_template('home.html')
 
 @app.route('/cafe_login')
@@ -149,7 +168,7 @@ def upload():
                 jam_buka = request.form['jam_buka']
                 jam_tutup = request.form['jam_tutup']
                 nama_lokasi =  request.form['nama_lokasi']
-                status = 'Buka'  # Ganti status sesuai dengan kebutuhan
+                status = 'Buka' 
                 if cafe.insertDB(filename, nama_cafe, Link_gmaps, deskripsi, jam_buka, jam_tutup, status, nama_lokasi):
                     flash('Cafe information successfully uploaded')
                 else:
@@ -160,7 +179,6 @@ def upload():
 def search_location():
     if request.method == 'POST':
         search_query = request.form['search_query']
-        # Ganti ini dengan fungsi pencarian yang sesuai dalam class Cafe Anda
         cafe_instance = Cafe()
         locations = cafe_instance.search_location(search_query) 
         return render_template('search_results_login.html', locations=locations, query=search_query)
@@ -170,16 +188,203 @@ def search_location():
 def search_location_nologin():
     if request.method == 'POST':
         search_query = request.form['search_query']
-        # Ganti ini dengan fungsi pencarian yang sesuai dalam class Cafe Anda
         cafe_instance = Cafe()
         locations = cafe_instance.search_location(search_query) 
         return render_template('search_results_nologin.html', locations=locations, query=search_query)
     return render_template('cafe_copy.html')
 
+@app.route('/cafe/<int:user_id>')
+def get_cafe_and_menu_by_user_id(user_id):
+    if 'user_id' not in session:
+        session['user_id'] = user_id
+    cafe_instance = Cafe()
+    cafes = cafe_instance.get_cafe_by_user_id(user_id)
+    if cafes:
+        cafe_id = cafes[0]['cafe_id']
+        print(cafe_id)
+        menu = cafe_instance.get_menu_by_cafe_id(cafe_id)
+        cafe_count = cafe_instance.count_cafes_by_user_id(user_id)
+        food_menu_count = cafe_instance.count_food_menus_by_cafe_id(cafe_id)
+        drink_menu_count = cafe_instance.count_drink_menus_by_cafe_id(cafe_id)
+        return render_template('index.html', cafes=cafes, menu=menu, cafe_count=cafe_count, food_menu_count=food_menu_count, drink_menu_count=drink_menu_count)
+    else:
+        return "No cafes found for this user."
+
+@app.route('/add_cafe_owner', methods=['GET', 'POST'])
+def add_cafe_owner():
+    if request.method == 'GET':
+        return render_template('owener_add_cafe.html')
+    if request.method == 'POST':
+        nama = request.form['nama']
+        nama_lokasi = request.form['nama_lokasi']
+        Link_gmaps = request.form['Link_gmaps']
+        deskripsi = request.form['deskripsi']
+        jam_buka = request.form['jam_buka']
+        jam_tutup = request.form['jam_tutup']
+        status = request.form['status']
+        fasilitas = request.form['fasilitas']  
+        user_id = session.get('user_id')  
+        cafe_instance = Cafe()
+        success = cafe_instance.insertDB(nama, nama_lokasi, Link_gmaps, deskripsi, jam_buka, jam_tutup, status, user_id, fasilitas)
+        if success:
+            return "Data cafe berhasil ditambahkan!"
+        else:
+            return "Gagal menambahkan data cafe."
+
+
+@app.route('/edit_cafe')
+def edit_cafe():
+    user_id = session.get('user_id')
+    if user_id:
+        cafe_instance = Cafe()
+        cafes = cafe_instance.get_cafe_by_user_id(user_id)
+        if cafes:
+            return render_template('owener_edit_cafe.html', all_cafes=cafes)
+        else:
+            return "No cafes found for this user."
+    else:
+        return "User ID not found in session."
+
+@app.route('/add_menu', methods=['GET', 'POST'])
+def add_menu():
+    if request.method == 'GET':
+        return render_template('owener_add_menu.html')
+
+    elif request.method == 'POST':
+        cafe_id = request.form['cafe_id']
+        category = request.form['category']
+        menu_name = request.form['menu_name']
+        price = request.form['price']
+        cafe_instance = Cafe()
+        success = cafe_instance.add_menu(cafe_id, category, menu_name, price)
+        if success:
+            flash("Menu added successfully")
+        else:
+            flash("Failed to add menu")
+        return redirect(url_for('add_menu', cafe_id=cafe_id))
+
+
+@app.route('/delete_cafe_owner', methods=['POST'])
+def delete_cafe_owner():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user_instance = Cafe()
+        success = user_instance.delete_cafe_owner_by_user_id(user_id)
+        if success:
+            flash(f"Data cafe owner dengan user_id {user_id} berhasil dihapus.", 'success')
+            return redirect(url_for('get_cafe_and_menu_by_user_id', user_id=user_id))
+        else:
+            flash(f"Gagal menghapus data cafe owner dengan user_id {user_id}.", 'error')
+            return redirect(url_for('get_cafe_and_menu_by_user_id', user_id=user_id)) 
+    else:
+        print(f"user_id: {user_id}")  
+        return render_template('index.html', user_id=user_id)
+
+@app.route('/all_cafes')
+def all_cafes():
+    cafe_instance = Cafe()
+    all_cafes = cafe_instance.get_all_data()
+    print(all_cafes)  
+    return render_template('admin_recomendasi_cafe.html', all_cafes=all_cafes)
+
+@app.route('/update', methods=['POST', 'GET'])
+def update():
+    if request.method == 'POST':
+        try:
+            cafe_id = request.form['id']
+            nama = request.form['nama']
+            link_gmaps = request.form['Link_gmaps']
+            deskripsi = request.form['deskripsi']
+            jam_buka = request.form['jam_buka']
+            jam_tutup = request.form['jam_tutup']
+            status = request.form['status']
+            nama_lokasi = request.form['nama_lokasi']
+            rekomendasi_user = request.form['rekomendasi_user']
+            user_id = request.form['user_id']
+            fasilitas = request.form['fasilitas']
+
+            cafe_instance = Cafe()
+            updated = cafe_instance.update_cafe(
+                cafe_id, nama, link_gmaps, deskripsi, jam_buka, jam_tutup, status, nama_lokasi, rekomendasi_user, user_id, fasilitas
+            )
+            if updated:
+                flash("Data Updated Successfully")
+                return redirect(url_for('all_cafes'))
+            else:
+                flash("Failed to update data")
+                return redirect(url_for('all_cafes'))
+
+        except Exception as e:
+            print("An error occurred during update:", str(e))
+            flash("Failed to update data")
+            return redirect(url_for('all_cafes'))
+
+@app.route('/tambah_cafe', methods=['GET', 'POST'])
+def tambah_cafe():
+    if request.method == 'GET':
+        return redirect(url_for('all_cafes'))
+    elif request.method == 'POST':
+        nama = request.form['nama']
+        link_gmaps = request.form['Link_gmaps']
+        deskripsi = request.form['deskripsi']
+        jam_buka = request.form['jam_buka']
+        jam_tutup = request.form['jam_tutup']
+        status = request.form['status']
+        nama_lokasi = request.form['nama_lokasi']
+        rekomendasi_user = request.form['rekomendasi_user']
+        user_id = request.form.get('user_id')
+        fasilitas = request.form['fasilitas']
+        cafe_model = Cafe()
+        inserted = cafe_model.insert_cafe(
+            nama, link_gmaps, deskripsi, jam_buka, jam_tutup, status, nama_lokasi, rekomendasi_user, user_id, fasilitas
+        )
+        if inserted:
+            flash("Data Cafe Ditambahkan")
+            return redirect(url_for('tambah_cafe'))
+        else:
+            flash("Gagal Menambahkan Data Cafe")
+            return redirect(url_for('tambah_cafe'))
+
+@app.route('/delete/<string:id_data>', methods=['GET'])
+def delete(id_data):
+    cafe_instance = Cafe()
+    if cafe_instance.delete_cafe_by_id(id_data):
+        flash("Record Has Been Deleted Successfully", "success")
+    else:
+        flash("Failed to delete record", "danger")
+    return redirect(url_for('all_cafes'))
+
+
+@app.route('/delete_user/<int:user_id>', methods=['GET', 'POST'])
+def delete_user(user_id):
+    if request.method == 'POST':
+        user_model = user()
+        deleted = user_model.delete_user(user_id)
+        if deleted:
+            flash('Pengguna berhasil dihapus', 'success')  
+        else:
+            flash('Gagal menghapus pengguna', 'error')  
+        return redirect(url_for('get_all_users'))
+    else:
+        return redirect(url_for('get_all_users',user_id=user_id))
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    user_model = user()
+    all_users = user_model.get_all_users()
+    if all_users:
+        return render_template('admin_kelola_pengguna.html', users=all_users)
+    else:
+        flash('Tidak ada data pengguna', 'info') 
+        return render_template('admin_kelola_pengguna.html', users=[])
+
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
+    session.pop('user_id', None)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
